5a97b54e4bbd64d6ffc71902afc8bd214d4a6811
rpcach <rpcach@users.noreply.github.com>
2015-12-10 12:23:50 -0500

    solved 32

---
+++ 32.py
@@ -0,0 +1,34 @@
+import math
+
+def main():
+	panDigits = []
+
+	for a in range(1,10000):
+		for b in range(a,10000):
+			c = a*b
+			if isPandigital(a,b,c):
+				if c not in panDigits:
+					panDigits.append(c)
+					print(panDigits)
+	
+	sum = 0
+	for x in array:
+		sum += x
+
+	print(sum)
+
+		
+def isPandigital(a,b,c,):
+	abc = str(a) + str(b) + str(c)
+	digits = [1,2,3,4,5,6,7,8,9]
+	
+	if len(abc) != 9:
+		return 0
+
+	for x in digits:
+		if str(x) not in abc:
+			return 0
+
+	return 1
+
+main()
