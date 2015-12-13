4d1585146e8cff1e3c2be28a2635f6f86451a281
rpcach <rpcach@users.noreply.github.com>
2015-12-12 21:12:48 -0500

    solved 46

---
+++ 46.py
@@ -0,0 +1,36 @@
+import math
+
+def main():
+	array = []
+
+	for x in range(1,100000):
+		array.append(2*x*x)
+
+	for x in range(3,100000,2):
+		if not isPrime(x) and not conjectureWorks(x,array):
+			print(x)
+			break
+
+def conjectureWorks(n,array):
+	for y in array:
+		if isPrime(n-y):
+			return 1
+
+	return 0
+
+def isPrime(n):
+	if n < 2:
+		return 0
+
+	if n == 2:
+		return 1
+
+	if n%2 == 0:
+		return 0
+
+	for x in xrange(3,int(math.sqrt(n))+1,2):
+		if n % x == 0:
+			return 0
+	return 1
+
+main()
